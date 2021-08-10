63af1e48075294dfbb8e48cd1f31ba6ac251b1dc
ivan.silva <ivan.elisandro@gmail.com>
2021-08-09 21:26:06 -0300

    CHANGED: Implementation of Stage 2 of the HyperJob Agency project. Created during my studies using JetBrains Academy.
    This stage only created the menu UI of the application.
    
    Important files:
    HyperJob Agency/task/hyperjob/settings.py
    HyperJob Agency/task/hyperjob/urls.py
    HyperJob Agency/task/menu/templates/menu/base.html
    HyperJob Agency/task/menu/templates/menu/index.html
    HyperJob Agency/task/menu/views.py
    HyperJob Agency/task/static/css/base.css

--- HyperJob Agency/task/hyperjob/urls.py
+++ HyperJob Agency/task/hyperjob/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from menu.views import MenuView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', MenuView.as_view()),
 ]
+urlpatterns += static(settings.STATIC_URL)
